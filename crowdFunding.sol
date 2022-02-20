b3d755b49f7ebb71d6c2664b93f0ac80e6b332d5
ManishVerma16 <vermam318@gmail.com>
2022-02-20 23:58:20 +0530

    constructor created

--- crowdFunding.sol
+++ crowdFunding.sol
@@ -1,1 +1,18 @@
-pragma solidity >=0.6.0  <0.9.0;
+pragma solidity >=0.6.0 <0.9.0;
+
+contract CrowdFunding {
+    mapping(address=>uint) public contributors;
+    address public manager;
+    uint public minimumContribution;
+    uint public deadline;
+    uint public target;
+    uint public raisedAmount;
+    uint public noOfContributors;
+
+    constructor(uint _target,uint _deadline){
+        target = _target;
+        deadline = _deadline + block.timestamp;
+        minimumContribution = 100 wei;
+        manager = msg.sender;
+    }
+}
