24379bfa3744263081286241cf9ccdd9615caec0
ManishVerma16 <vermam318@gmail.com>
2022-02-20 17:15:05 +0530

    initial commit

---
+++ simpleStorage.sol
@@ -0,0 +1,14 @@
+pragma solidity >=0.6.0 <0.9.0;
+// pragma solidity ^0.6.0; // set to specific version of solidity
+
+contract SimpleStorage {
+
+    // datatypes
+    uint256 favoriteNumber = 5;  // unsigned integer
+    bool favoriteBool = true;   // boolean
+    string favoriteString = "Test String";  // string
+    int256 favoriteInt = -5;    // can be +ve or -ve integer
+    address favoriteAddress = 0xF80C601223C45f88da0683841BFc651a11E69090; // hex address
+    bytes32 favoriteByte = "cat";   //
+    
+}
