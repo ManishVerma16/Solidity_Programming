28d99c436f7984757a605993a1c1c55238bdabe1
ManishVerma16 <vermam318@gmail.com>
2022-02-22 23:58:36 +0530

    structs

--- crowdFunding.sol
+++ crowdFunding.sol
@@ -1,3 +1,4 @@
+//SPDX-license-Identifier: UNLICENSED
 pragma solidity >=0.6.0 <0.9.0;
 
 contract CrowdFunding {
@@ -9,10 +10,42 @@ contract CrowdFunding {
     uint public raisedAmount;
     uint public noOfContributors;
 
-    constructor(uint _target,uint _deadline){
+    struct Request{
+        string description;
+        address payable;
+        uint value;
+        bool completed;
+        uint noOfVoters;
+        mapping(address=>bool) voters;
+    }
+    mapping(uint => Request) public request;
+    uint public numRequest;
+
+    constructor (uint _target,uint _deadline) public {
         target = _target;
         deadline = _deadline + block.timestamp;
         minimumContribution = 100 wei;
         manager = msg.sender;
+    }
+
+    function sendEth() public payable{
+        require(block.timestamp < deadline, "Deadline has passed");
+        require(msg.value >= minimumContribution, "Minimum Contribution is not met");
+        if(contributors[msg.sender] == 0){
+            noOfContributors +=1;
+        }
+        contributors[msg.sender] += msg.value;
+        raisedAmount += msg.value;
+    }
+    function getContractBalance() public view returns(uint){
+        return address(this).balance;
+    }
+
+    function refund() public {
+        require(block.timestamp>deadline && raisedAmount<target, "You are not eligible for refund");
+        require(contributors[msg.sender]>0);
+        address payable user = payable(msg.sender);
+        user.transfer(contributors[msg.sender]);
+        contributors[msg.sender] = 0;
     }
 }
